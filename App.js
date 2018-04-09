42517244ac0d63ffc56da07ed08b893ed1f476c8
Dan Naeem <dan.naeem95@gmail.com>
2018-04-09 19:01:12 -0400

    push test version of the UI

---
+++ App.js
@@ -0,0 +1,97 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Alert
+} from 'react-native';
+import {Button} from 'react-native-elements';
+import { StackNavigator } from 'react-navigation';
+
+import HomeScreen from './components/HomeScreen';
+//import AllRoutes from './components/RootStack';
+import HelloWorld from './components/HelloWorld';
+import ErrorPage from './components/ErrorPage';
+import MyHeader from './components/MyHeader.js';
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Error_Page: {
+      screen: ErrorPage,
+    },
+    Hello_Page: {
+      screen: HelloWorld,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <RootStack />
+    );
+  }
+}
+
+
+/*
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.heading}>
+          L33T!
+        </Text>
+        <View style={styles.space}></View>
+        <Button
+          title="Test Button"
+          onPress={() => {
+            Alert.alert('Test Complete');
+          }}
+          />
+      </View>
+    );
+  }
+}
+//*/
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  heading: {
+    color: 'red',
+    fontSize: 60,
+    textAlign: 'center',
+    margin: 10,
+    paddingTop: 5,
+    borderWidth: 5,
+    borderColor: 'blue',
+    borderRadius:20,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  space: {
+    margin: 30,
+  }
+});
